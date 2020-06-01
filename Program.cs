9d2a0cdb0042f0a1c19e05a6436954d2e5deadaa
Chihiro-0703 <c-watanabe@itnet.co.jp>
2020-06-01 12:21:37 +0900

    happy monday

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ITO_DX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Adam!");
+        }
+    }
+}
